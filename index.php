8f025e35979586199d12ea93f44d431c0395827a
Jocevaldo Barreto <jocevaldo.barreto@llumbronzearte.com.br>
2018-10-03 18:01:04 -0300

    Criado mais Classe usuario

--- index.php
+++ index.php
@@ -11,11 +11,30 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
 
-
+/*
+// Carrega um usuario
 $root = new Usuario();
 $root->loadByID(3);
-
 echo $root;
+*/
+
+/*
+//Carrega uma Lista de usuarios
+$lista = Usuario::getList();
+echo json_encode($lista);
+ */
+
+/*
+//Carrega uma lista de usuarios buscando pelo login
+$search = Usuario::search("jo");
+echo json_encode($search);
+*/
+
+// Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Usuario","123");
+echo $usuario;
+
 
  ?>
 
